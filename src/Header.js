f739cb86f14121a45e8859f13ff8738a5e5055e9
Feliphe Paz <feliphe.s.paz@gmail.com>
2021-11-05 15:57:59 -0300

    menu mobile modificado

--- src/Header.js
+++ src/Header.js
@@ -3,18 +3,23 @@ import logoMain from './Assets/logo.png';
 import './Header.css';
 import { NavLink } from 'react-router-dom';
 import menu from './Assets/menu.png';
+import menuAlt from './Assets/menu-alt.png'
 import logoAlt from './Assets/logo-alt.png';
 import cancel from './Assets/cancel.png';
+import cancelAlt from './Assets/cancel-alt.png';
 
 window.addEventListener('scroll', () => {
+  const btnMobile = document.querySelector('.btn-mobile img');
   const header = document.querySelector('.header');
   const logo = document.querySelector('.logo img');
   if (window.pageYOffset > 50) {
     header.classList.add('active-scroll');
     logo.src = logoAlt;
+    btnMobile.src = menuAlt;
   } else {
     header.classList.remove('active-scroll');
     logo.src = logoMain;
+    btnMobile.src = menu;
   }
 })
 
@@ -25,9 +30,15 @@ function handleMobile(e) {
   if (element.classList.contains('active-mobile')) {
     element.children[0].src = cancel;
     menuMobile.style.display = 'block';
+    if (window.pageYOffset > 50) {
+      element.children[0].src = cancelAlt;
+    }
   } else {
     element.children[0].src = menu;
     menuMobile.style.display = 'none';
+    if (window.pageYOffset > 50) {
+      element.children[0].src = menuAlt;
+    }
   }
 }
 
