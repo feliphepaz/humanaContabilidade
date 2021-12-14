0d9d84031e323ded7672ed08338b1dd415e7c8d9
Feliphe Paz <feliphe.s.paz@gmail.com>
2021-12-13 22:12:29 -0300

    navegação adicionada nos depoimentos

--- src/Components/ProvaSocial.js
+++ src/Components/ProvaSocial.js
@@ -2,7 +2,31 @@ import React from 'react';
 import './ProvaSocial.css';
 import patricia from '../Assets/patricia.png';
 import daniele from '../Assets/daniele.png';
+import marcia from '../Assets/marcia.png';
+import fprati from '../Assets/fprati.png';
+import adayr from '../Assets/adayr.png';
+import roberta from '../Assets/roberta.png';
+import danilo from '../Assets/gaba.png';
 import star from '../Assets/star.png';
+
+let n = 0;
+function handleDepoNav(e) {
+    if (e.target.innerText === '›') {
+        n += 1;
+    } else if (e.target.innerText === '‹') {
+        n -= 1;
+    }
+    if (n > 3) {
+        n = 0;
+    } else if (n < 0) {
+        n = 3;
+    }
+    const documentaries = document.querySelectorAll('.documentaries');
+    documentaries.forEach((doc) => {
+        doc.style.display = 'none';
+    });
+    documentaries[n].style.display = 'flex';
+}
 
 const perfis = [
     {
@@ -20,7 +44,47 @@ const perfis = [
         foto: patricia,
         avaliação: 5,
         comentario: 'A empresa Humana sempre supre 100% das minhas necessidades e solicitações. Prestam um atendimento com profissionalismo, praticidade, simpatia. Nunca fico sem um rápido retorno. Recomendo.',
-    }
+    },
+    {
+        id: 3,
+        nome: 'Marcia Bestel',
+        ocupação: 'Diretora da Brooklin',
+        foto: marcia,
+        avaliação: 5,
+        comentario: 'A Humana é uma empresa referência em contabilidade! Não tomamos nenhuma decisão contábil, sem consultar a sócia fundadora. Sabem ouvir o cliente, entender suas necessidades e, por esta razão, oferecem um serviço personalizado e diferenciado.',
+    },
+    {
+        id: 4,
+        nome: 'Fábio Prati',
+        ocupação: 'Diretor da F Prati Consulting',
+        foto: fprati,
+        avaliação: 5,
+        comentario: 'Nesse setor complexo devido nossa legislação, é difícil encontrar as respostas corretas, e isso eu tenho obtido da Humana, reduzindo minha exposição a riscos e mantendo minha empresa em dia com suas obrigações.',
+    },
+    {
+        id: 5,
+        nome: 'Adayr Barros Jr',
+        ocupação: 'Diretor da Caema Brasil Consultoria',
+        foto: adayr,
+        avaliação: 5,
+        comentario: 'A Humana é uma excelente empresa de contabilidade. Depois de muitas decepções no mercado, finalmente encontrei uma empresa séria, competente e rápida nas respostas.',
+    },
+    {
+        id: 6,
+        nome: 'Roberta Mustacchi',
+        ocupação: 'Diretora da CEPEC-SP',
+        foto: roberta,
+        avaliação: 5,
+        comentario: 'A 1 ano assumi a clínica médica de meu pai, quando me deparei com uma desorganização vinda por parte do antigo contador. Recebi a indicação de procurar a Dilma e fui supreendida por um serviço de excelência. Hoje sinto-me segura e satisfeita.',
+    },
+    {
+        id: 7,
+        nome: 'Danilo Gaba',
+        ocupação: 'C.E.O da Equilibriumm Consultório Psicológico',
+        foto: danilo,
+        avaliação: 5,
+        comentario: 'Sou grato em ser cliente da Dilma. Confesso ter sido resistente antes do primeiro contato, por sempre ter sido autônomo, mas hoje reconheço ser melhor este formato. Graças a sua paciência e dedicação no apoio das minhas dúvidas e inseguranças.',
+    },
 ]
 
 const ProvaSocial = () => {
@@ -50,11 +114,82 @@ const ProvaSocial = () => {
         </section>
 
         <section className='social-proof'>
-            <div className='container'>
-                <h3>Clientes Satisfeitos</h3>
-                <p>Veja o que estão falando de nós.</p>
-                <ul className='documentaries'>
-                    {perfis.map((per) => (
+                <div className='container'>
+                    <div className='depo-head'>
+                        <div className='depo-head-info'>
+                            <h3>Clientes Satisfeitos</h3>
+                            <p>Veja o que estão falando de nós.</p>
+                        </div>
+                        <nav className='nav-depo'>
+                            <button onClick={handleDepoNav}>‹</button>
+                            <button onClick={handleDepoNav}>›</button>
+                        </nav>
+                </div>
+                <ul className='documentaries first-doc' id='0'>
+                    {perfis.slice(0, 2).map((per) => (
+                        <li key={per.id}>
+                            <div className='profile'>
+                                <div>
+                                    <img src={per.foto} alt=''></img>
+                                </div>
+                                <div>
+                                    <h4>{per.nome}</h4>
+                                    <span>{per.ocupação}</span>
+                                </div>
+                            </div>
+                            <p>{per.comentario}</p>
+                            <div className='rating'>
+                                {[...Array(per.avaliação)].map((item, index) => (
+                                    <img key={index} src={star} alt=''></img>
+                                ))}
+                            </div>
+                        </li>
+                    ))}
+                    </ul>
+                <ul className='documentaries' id='1'>
+                    {perfis.slice(2, 4).map((per) => (
+                        <li key={per.id}>
+                            <div className='profile'>
+                                <div>
+                                    <img src={per.foto} alt=''></img>
+                                </div>
+                                <div>
+                                    <h4>{per.nome}</h4>
+                                    <span>{per.ocupação}</span>
+                                </div>
+                            </div>
+                            <p>{per.comentario}</p>
+                            <div className='rating'>
+                                {[...Array(per.avaliação)].map((item, index) => (
+                                    <img key={index} src={star} alt=''></img>
+                                ))}
+                            </div>
+                        </li>
+                    ))}
+                    </ul>
+                <ul className='documentaries' id='2'>
+                    {perfis.slice(4, 6).map((per) => (
+                        <li key={per.id}>
+                            <div className='profile'>
+                                <div>
+                                    <img src={per.foto} alt=''></img>
+                                </div>
+                                <div>
+                                    <h4>{per.nome}</h4>
+                                    <span>{per.ocupação}</span>
+                                </div>
+                            </div>
+                            <p>{per.comentario}</p>
+                            <div className='rating'>
+                                {[...Array(per.avaliação)].map((item, index) => (
+                                    <img key={index} src={star} alt=''></img>
+                                ))}
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+                <ul className='documentaries' id='3'>
+                    {perfis.slice(6).map((per) => (
                         <li key={per.id}>
                             <div className='profile'>
                                 <div>
--- src/Parceiros.js
+++ src/Parceiros.js
@@ -6,13 +6,6 @@ import fpc from './Assets/fpc.png';
 import caema from './Assets/caema.png';
 import patriciasalmona from './Assets/patriciasalmona.png';
 import cepec from './Assets/cepec.png';
-import marcia from './Assets/marcia.png';
-import daniele from './Assets/daniele.png';
-import fprati from './Assets/fprati.png';
-import adayr from './Assets/adayr.png';
-import patricia from './Assets/patricia.png';
-import roberta from './Assets/roberta.png';
-import gaba from './Assets/gaba.png';
 import equilibriumm from './Assets/equilibriumm.png';
 
 const Parceiros = () => {
@@ -30,18 +23,6 @@ const Parceiros = () => {
                         </div>
                         <div>
                             <p>Em atuação no Brasil há 20 anos, a Brooklin é uma seguradora especializada em seguros de vida e na venda personalizada.</p>
-                            <div className='documentario'>
-                                <div className='documentario-perfil'>
-                                    <div>
-                                        <img src={marcia} alt=''></img>
-                                    </div>
-                                    <div>
-                                        <h4>Marcia Bestel</h4>
-                                        <span>Diretora da Brooklin</span>
-                                    </div>
-                                </div>
-                                <p>A Humana é uma empresa referência em contabilidade! Não tomamos nenhuma decisão contábil, sem consultar a sócia fundadora.</p>
-                            </div>
                         </div>
                     </li>
                     <li>
@@ -50,18 +31,6 @@ const Parceiros = () => {
                         </div>
                         <div>
                             <p>A Dape Capital é uma boutique de investimentos em criptomoedas, pautada em um conceito Private de atendimento e gestão de patrimônio.</p>
-                            <div className='documentario'>
-                                <div className='documentario-perfil'>
-                                    <div>
-                                        <img src={daniele} alt=''></img>
-                                    </div>
-                                    <div>
-                                        <h4>Daniele Abdo</h4>
-                                        <span>C.E.O da Dape Capital</span>
-                                    </div>
-                                </div>
-                                <p> Sempre fui bem atendida e a cada nova demanda, a Humana vem me atendendo e superando minhas expectativas!</p>
-                            </div>
                         </div>
                     </li>
                     <li>
@@ -70,18 +39,6 @@ const Parceiros = () => {
                         </div>
                         <div>
                             <p>A F Prati Consulting é uma empresa que oferece serviços de engenharia aos seus clientes, no ramo de consultoria em construções.</p>
-                            <div className='documentario'>
-                                <div className='documentario-perfil'>
-                                    <div>
-                                        <img src={fprati} alt=''></img>
-                                    </div>
-                                    <div>
-                                        <h4>Fábio Prati</h4>
-                                        <span>Diretor da F Prati Consulting</span>
-                                    </div>
-                                </div>
-                                <p>Nesse setor complexo devido nossa legislação, é difícil encontrar as respostas corretas, e isso eu tenho obtido da Humana, reduzindo minha exposição a riscos e mantendo minha empresa em dia com suas obrigações.</p>
-                            </div>
                         </div>
                     </li>
                     <li>
@@ -90,18 +47,6 @@ const Parceiros = () => {
                         </div>
                         <div>
                             <p>A Caema Brasil é uma empresa voltada à consultoria técnica em engenharia de materiais e novas tecnologias.</p>
-                            <div className='documentario'>
-                                <div className='documentario-perfil'>
-                                    <div>
-                                        <img src={adayr} alt=''></img>
-                                    </div>
-                                    <div>
-                                        <h4>Adayr Borro Jr</h4>
-                                        <span>Diretor da Caema Brasil Consultoria</span>
-                                    </div>
-                                </div>
-                                <p>A Humana é uma excelente empresa de contabilidade. Depois de muitas decepções no mercado, finalmente encontrei uma empresa séria, competente e rápida nas respostas.</p>
-                            </div>
                         </div>
                     </li>
                     <li>
@@ -110,18 +55,6 @@ const Parceiros = () => {
                         </div>
                         <div>
                             <p>Dra. Patrícia Salmona é médica pediatra e geneticista especializada em Síndrome de Down. Atualmente comanda a Salmona Clínica Médica.</p>
-                            <div className='documentario'>
-                                <div className='documentario-perfil'>
-                                    <div>
-                                        <img src={patricia} alt=''></img>
-                                    </div>
-                                    <div>
-                                        <h4>Dra. Patrícia Salmona</h4>
-                                        <span>Diretora da Salmona Clínica Médica</span>
-                                    </div>
-                                </div>
-                                <p>A Humana supre 100% das minhas necessidades e solicitações. Prestam um atendimento com profissionalismo, praticidade, simpatia. Nunca fico sem um retorno rápido. Recomendo.</p>
-                            </div>
                         </div>
                     </li>
                     <li>
@@ -130,18 +63,6 @@ const Parceiros = () => {
                         </div>
                         <div>
                             <p>A CEPEC-SP é uma clínica de atendimento pediátrico e genético, com foco em Trissomia 21 (Síndrome de Down).</p>
-                            <div className='documentario'>
-                                <div className='documentario-perfil'>
-                                    <div>
-                                        <img src={roberta} alt=''></img>
-                                    </div>
-                                    <div>
-                                        <h4>Roberta Mustacchi</h4>
-                                        <span>Diretora da CEPEC-SP</span>
-                                    </div>
-                                </div>
-                                <p>A 1 ano assumi a clínica médica de meu pai, quando me deparei com uma desorganização vinda por parte do antigo contador. Recebi a indicação de procurar a Dilma e fui supreendida por um serviço de excelência. Hoje sinto-me segura e satisfeita.</p>
-                            </div>
                         </div>
                     </li>
                     <li>
@@ -150,18 +71,6 @@ const Parceiros = () => {
                         </div>
                         <div>
                             <p>A Equilibriumm nasceu em 2012 com o propósito de levar a seus pacientes recursos terapêuticos  que proporcionem uma melhor qualidade de vida.</p>
-                            <div className='documentario'>
-                                <div className='documentario-perfil'>
-                                    <div>
-                                        <img src={gaba} alt=''></img>
-                                    </div>
-                                    <div>
-                                        <h4>Danilo Gaba</h4>
-                                        <span>C.E.O da Equilibriumm Consultório Psicológico</span>
-                                    </div>
-                                </div>
-                                <p>Sou grato em ser cliente da Dilma. Confesso ter sido resistente antes do primeiro contato, por sempre ter sido autônomo, mas hoje reconheço ser melhor este formato. Graças a sua paciência e dedicação no apoio das minhas dúvidas e inseguranças.</p>
-                            </div>
                         </div>
                     </li>
                 </ul>
