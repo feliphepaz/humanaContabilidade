ddddfee0575cebca2ade6004a03a9f7e1bd257a0
Feliphe Paz <feliphepaz@MacBook-Air-de-Feliphe.local>
2022-04-06 11:28:32 -0300

    ajuste no IRPF22

--- src/IRPF2022.js
+++ src/IRPF2022.js
@@ -12,7 +12,7 @@ const IRPF2022 = () => {
                     <h3>Quem deve declarar o Imposto de Renda 2022?</h3>
                     <p>O imposto de renda funciona como uma tributação federal, anualmente gerenciada pelo RF. Sua prestação é obrigatória para todos aqueles que tenham tido rendimentos tributáveis superiores a R$ 28.559. No entanto, é preciso ficar atento as regras e documentações.</p>
 
-                    <h3>Prazo para entrega de informe de rendimentos vai até dia 31</h3>
+                    <h3>O prazo de entrega da declaração do Imposto de Renda 2022</h3>
                     <p>O Imposto de Renda em 2022, que terá início no próximo dia 7 de março e vai se encerrar dia 31 de maio.</p>
 
                     <h3>Quem precisa declarar o IRPF em 2022?</h3>
